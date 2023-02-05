9ece5039e8458a1c55a266d325f17102d85cef4d
Seohyun Yang <seoh136199@gmail.com>
2023-02-06 00:25:33 +0900

    put 구현

--- api/user/user.py
+++ api/user/user.py
@@ -33,8 +33,30 @@ class UserManagement(Resource):
 
 
     def put(self):
-        # PUT method 구현 부분
-        return {}
+        # 닉네임 변경
+        id = request.get_json()["id"]
+        pw = request.get_json()["password"]
+        nn = request.get_json()["nickname"]
+
+        db = Database()
+        sql = "SELECT * FROM user where id = %s"
+        result = db.execute_one(sql, (id))
+
+        if (result == None or result['password'] != pw):
+            db.commit()
+            db.close()
+            return { "is_success": False, "message": "아이디나 비밀번호 불일치" }, 400
+        elif (result['nickname'] != nn):
+            db.commit()
+            db.close()
+            return { "is_success": False, "message": "현재 닉네임과 같음" }, 400
+        else:
+            sql = "UPDATE user SET nickname=%s WHERE id = %s"
+            db.execute(sql, (nn, id))
+            db.commit()
+            db.close()
+            return { "is_success": True, "message": "유저 닉네임 변경 성공 성공" }, 200
+    
     
     def delete(self):
         # 유저 삭제
@@ -48,10 +70,10 @@ class UserManagement(Resource):
         if (result == None or result['password'] != pw):
             db.commit()
             db.close()
-            return { "is_success": False, "message": "아이디나 비밀번호 불일치" }, 
+            return { "is_success": False, "message": "아이디나 비밀번호 불일치" }, 400
         else:
             sql = "DELETE FROM user WHERE id = %s"
             db.execute(sql, (id))
             db.commit()
             db.close()
-            return { "is_success": True, "message": "유저 삭제 성공" }, 400
+            return { "is_success": True, "message": "유저 삭제 성공" }, 200
