5472fe1a49a26e371c24a506ba240296ce2ab62f
Seohyun Yang <seoh136199@gmail.com>
2023-02-05 23:26:48 +0900

    self 인자 수정

--- api/user/user.py
+++ api/user/user.py
@@ -17,11 +17,11 @@ class UserManagement(Resource):
         nn = request.get_json()["nickname"]
 
         sql = "SELECT * FROM user where id = %s"
-        result = Database.execute_one(sql, (id))
+        result = Database.execute_one(Database, sql, (id))
 
         if (result.count == 0):
             sql = "INSERT INTO user (id, password, nickname) VALUES (%s, %s, %s)"
-            Database.execute(sql, (id, pw, nn))
+            Database.execute(Database, sql, (id, pw, nn))
             return { "is_success": True, "message": "유저 생성 성공" }, 200
         else:
             return { "is_success": False, "message": "이미 있는 유저" }, 400
